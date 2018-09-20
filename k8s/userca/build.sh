6c76392780d9e49b02bb39ac36a5b28cd3013ad3
Your Name <you@example.com>
2018-09-20 15:42:26 +0800

    add ca user create

---
+++ k8s/userca/build.sh
@@ -0,0 +1,7 @@
+#Used to create a ca for client and run a test
+cp /etc/kubernetes/ssl/ca* ./
+openssl genrsa -out client.key 2048
+openssl req -new -key client.key -out client.csr -config csr.conf
+openssl x509 -req -in client.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out client.crt -days 10000  -extfile csr.conf
+kubectl create -f user.yaml 
+curl -v https://10.129.11.80:6443/  --cert ./client.crt --key ./client.key  --cacert ca.pem
