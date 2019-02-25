888504272fe479d2b185241cef91ccb814932b88
meihongwei <xiyanxiyan10@hotmail.com>
2019-02-25 21:09:48 +0800

    update pkg file

--- tools/pkg.sh
+++ tools/pkg.sh
@@ -1,2 +1,6 @@
-rm quant.tar.gz
-tar cvf quant-$(date "+%Y-%m-%d %H:%M:%S").tar.gz quant
+ts=$(date "+%Y%m%d")
+fs=gogs-${ts}.tar.gz 
+echo $ts
+echo $fs
+tar cvf ${fs} gogs 
+mv ${fs} /Users/xiyanxiyan10/Desktop/${fs}
