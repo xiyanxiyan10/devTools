0e45bbc8583e281f4d2f3d3e90193f71847371ec
xiyanxiyan10 <xiyanxiyan10@hotmail.com>
2019-10-10 14:50:52 +0800

    reset the run scripts

--- tools/registry/run.sh
+++ tools/registry/run.sh
@@ -1,6 +1,16 @@
-docker stop registry
-docker rm registry
-docker run -d -p 443:443 --restart=always --name registry -v /certs:/certs -e REGISTRY_HTTP_ADDR=0.0.0.0:443 -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/registry.crt -e REGISTRY_HTTP_TLS_KEY=/certs/registry.key registry:2
+docker run -d -p 443:443 \
+    --restart=always \
+    --name registry \
+    -v /certs:/certs \
+    -v /auth:/auth \
+    -v /registry:/var/lib/registry \
+    -e REGISTRY_HTTP_ADDR=0.0.0.0:443\
+    -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/registry.crt \
+    -e REGISTRY_HTTP_TLS_KEY=/certs/registry.key \
+    -e REGISTRY_AUTH_HTPASSWD_REALM="Registry Realm"\
+    -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd\
+    -e "REGISTRY_AUTH=htpasswd"\
+    registry:2
 
 
 
