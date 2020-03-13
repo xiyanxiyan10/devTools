c1a1ed1622808d72d01fd3d390810487e49493fe
hongwei.mei <hongwei.mei@changhong.com>
2020-03-13 16:02:13 +0800

    1.add server.key create pocess

--- k8s/hpa-custom/prepare.sh
+++ k8s/hpa-custom/prepare.sh
@@ -1,4 +1,5 @@
 cd /etc/kubernetes/ssl
+openssl genrsa -out serving.key 2048
 openssl req -new -key serving.key -out serving.csr -subj "/CN=serving"
 openssl  x509 -req -in serving.csr -CA ./ca.crt -CAkey ./ca.key -CAcreateserial -out serving.crt -days 3650
 kubectl create secret generic cm-adapter-serving-certs --from-file=serving.crt=./serving.crt --from-file=serving.key=./serving.key  -n  custom-metrics
